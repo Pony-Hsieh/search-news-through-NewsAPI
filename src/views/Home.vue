0828fcdc2651d4dedc055ab72c38c2cbd3c113d0
Pony Hsieh <lbjwin@gmail.com>
2021-07-12 23:15:42 +0800

    完成網頁基礎架構

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,123 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <input type="text" v-model.trim.lazy="searchRrequirement.newsKeyWord">
+
+        最早日期
+        <input type="date" v-model.lazy="searchRrequirement.dateRangeStart">
+
+        最晚日期
+        <input type="date" v-model.lazy="searchRrequirement.dateRangeEnd">
+
+        排序標準
+        <div>
+            <input type="radio" id="publishedAt" value="publishedAt" v-model="searchRrequirement.sortBy">
+            <label for="publishedAt">publishedAt</label>
+            <br>
+            <input type="radio" id="relevancy" value="relevancy" v-model="searchRrequirement.sortBy">
+            <label for="relevancy">relevancy</label>
+            <br>
+            <input type="radio" id="popularity" value="popularity" v-model="searchRrequirement.sortBy">
+            <label for="popularity">popularity</label>
+            <br>
+            <span>Picked: {{ searchRrequirement.sortBy }}</span>
+        </div>
+
+        <!-- <br />
+        <button type="button" @click="checkDate">checkDate</button> -->
+
+        <br />
+        <button type="button" @click="searchNewsByUser">searchNewsByUser</button>
+
+        <br />
+        https://newsapi.org/v2/everything
+
+        ?q=apple
+
+        &
+        from=2021-07-11
+
+        &
+        to=2021-07-11
+
+        &
+        sortBy=popularity
+
+        &
+        apiKey=ac708e48dd7a41368f864020a2374115
+
+
+
+        <div v-for="item in allNewsData" class="singleNews" :key="item.url">
+            <!-- 新聞圖片： -->
+            <img :src="item.urlToImage" alt="新聞圖片">
+
+            <h3>新聞標題</h3>
+            {{ item.title }}
+
+            <h4>概述</h4>
+            {{ item.description }}
+
+            <h4>發布時間</h4>
+            {{ item.publishedAt }}
+
+            <h4>作者</h4>
+            {{ item.author }}
+
+            <router-link :to="{ path: '/singleNews', query: { newsUrl: item.url }}"> singleNews </router-link>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    export default {
+        name: "home",
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+        data() {
+            return {
+                searchRrequirement: {
+                    newsKeyWord: "COVID-19",
+                    dateRangeStart: "",
+                    dateRangeEnd: "",
+                    sortBy: "publishedAt",
+                    page: 1,
+                },
+            }
+        },
+
+        computed: {
+            allNewsData() {
+                return this.$store.state.allNewsData;
+            },
+        },
+
+        mounted() {
+            this.$store.dispatch("getAllNews", this.searchRrequirement);
+        },
+
+        methods: {
+            checkDate() {
+                const D1 = new Date(this.searchRrequirement.dateRangeStart).getTime();
+                const D2 = new Date(this.searchRrequirement.dateRangeEnd).getTime();
+
+                // 兩者都有值
+                if (D1 && D2) {
+                    if (D1 > D2) {
+                        const oldDate = this.searchRrequirement.dateRangeEnd;
+                        const newDate = this.searchRrequirement.dateRangeStart;
+                        this.searchRrequirement.dateRangeStart = oldDate;
+                        this.searchRrequirement.dateRangeEnd = newDate;
+                    }
+                }
+            },
+
+            searchNewsByUser() {
+                this.checkDate();
+                this.$store.dispatch("getAllNews", this.searchRrequirement);
+            },
+        },
+    }
 </script>
+
+<style scoped lang="scss">
+    @import "@/assets/scss/home.scss";
+</style>
---
+++ src/views/SingleNews.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        ● 返回列表頁的按鈕(例如原本在第二頁，那就還是要維持在第二頁)
+        <button type="button" @click="toLastPage">上一頁</button>
+
+        ○ 新聞圖片 urlToImage
+        <!-- {{ singleNewsData.urlToImage }} -->
+        <img :src="singleNewsData.urlToImage" alt="新聞圖片">
+
+        <h3>新聞標題</h3>
+        {{ singleNewsData.title }}
+
+        <h4>發布時間</h4>
+        {{ singleNewsData.publishedAt }}
+
+        <h4>作者</h4>
+        {{ singleNewsData.author }}
+
+        <h4>內容</h4>
+        {{ singleNewsData.content }}
+        <a :href="singleNewsData.url" target="_blank">→ 前往原文網址閱讀</a>
+
+        <h4>來源名稱</h4>
+        {{ singleNewsData.source.name }}
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "singleNews",
+
+        computed: {
+            singleNewsData() {
+                return this.$store.state.singleNewsData;
+            },
+        },
+
+        mounted() {
+            this.$store.dispatch("filterSingleNews", this.$route.query.newsUrl);
+        },
+
+        methods: {
+            toLastPage() {
+                this.$router.go(-1);
+            },
+        },
+    }
+</script>
+
+<style scoped lang="scss">
+    @import "@/assets/scss/singleNews.scss";
+</style>
